e8b70ddb256274bb7561cf5273749c102afcb5bf
gregory6z <gregoryrag@gmail.com>
2022-03-17 23:15:21 +0100

    Creation of global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,10 @@
 
 import Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
-      <h1 className={styles.title}>ola</h1>
+      <h1>ola</h1>
       <Head>
         <title>ig.news | Page d'accueil</title>
       </Head>
