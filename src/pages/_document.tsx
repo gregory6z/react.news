8fd3ab357552de19ca1ace3012a0f0a951e88a01
gregory6z <gregoryrag@gmail.com>
2022-03-17 22:20:06 +0100

    dinamic title per page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,8 +9,6 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet" />
 
-        <title>ig.news</title>
-
       </Head>
       <body>
         <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
-/* eslint-disable import/no-unresolved */
+/* eslint-disable react/no-unescaped-entities */
+
+import Head from 'next/head';
+
 import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>ola</h1>
+    <>
+      <h1 className={styles.title}>ola</h1>
+      <Head>
+        <title>ig.news | Page d'accueil</title>
+      </Head>
+
+    </>
   );
 }
