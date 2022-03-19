868dca8c6b4bae88b6f2b2aad4a261de4b04d36a
gregory6z <gregoryrag@gmail.com>
2022-03-19 16:54:24 +0100

    component creation ( SignInButton )

---
+++ src/pages/components/SignInButton/index.tsx
@@ -0,0 +1,20 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#04d361" />
+      Gregory Praxedes
+      <FiX className={styles.closeIcon} color="#737380" />
+    </button>
+  ) : (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#eba417" />
+      Connecter avec GitHub
+    </button>
+  );
+}
