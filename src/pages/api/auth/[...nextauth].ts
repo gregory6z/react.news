685726e39a2bf3c4fcb0be3be8af9dfbec42255a
gregory6z <gregoryrag@gmail.com>
2022-03-23 20:33:20 +0100

    creating user check in faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -27,13 +27,33 @@ export default NextAuth({
       user, account, profile, email, credentials,
     }) {
       const Email = user;
-      await fauna.query(
-        q.Create(
-          q.Collection('users'),
-          { data: Email },
-        ),
-      );
-      return true;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email),
+                ),
+              ),
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { Email } },
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email),
+              ),
+            ),
+          ),
+        );
+        return true;
+      } catch {
+        return false;
+      }
     },
   },
 });
